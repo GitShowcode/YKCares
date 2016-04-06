36fb24e0d89fe930e08c6d6ad2425200d2e609b5
jiapeixin <18611631617@163.com>
2016-04-06 19:44:02 +0800

    11

---
+++ YKCares/YKCaresNoViewController.h
@@ -0,0 +1,35 @@
+//
+//  YKCaresNoViewController.h
+//  YKCares
+//
+//  Created by jiapeixin on 16/4/6.
+//  Copyright © 2016年 Gencare_Jia. All rights reserved.
+//
+
+#ifndef YKCaresNoViewController_h
+#define YKCaresNoViewController_h
+
+
+#define RGB(__r, __g, __b)  [UIColor colorWithRed:(1.0*(__r)/255)\
+green:(1.0*(__g)/255)\
+blue:(1.0*(__b)/255)\
+alpha:1.0]
+
+
+//分割线
+#define colorRGBA3 [UIColor colorWithRed:225.0/255 green:225.0/255 blue:225.0/255 alpha:1]
+//背景色
+#define colorRGBA9 [UIColor colorWithRed:248.0/255 green:248.0/255 blue:248.0/255 alpha:1]
+
+#define SCREEN_WIDTH ([UIScreen mainScreen].bounds.size.width)
+#define SCREEN_HEIGHT ([UIScreen mainScreen].bounds.size.height)
+
+#define JNormal UIControlStateNormal
+#define JAction UIControlEventTouchUpInside
+
+#define Fontsize(A) [UIFont systemFontOfSize:A]
+
+
+
+
+#endif /* YKCaresNoViewController_h */
--- YKCares/YKcaresHeader.h
+++ YKCares/YKcaresHeader.h
@@ -9,5 +9,13 @@
 #ifndef YKcaresHeader_h
 #define YKcaresHeader_h
 
+#define Baseurl @"http://182.92.107.14/index.php/Interface/"
+#define IMAGE(A) [UIImage imageNamed:A]
+
+#define BACK(block) dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), block)
+
+#define MAIN(block) dispatch_async(dispatch_get_main_queue(),block)
+
+
 
 #endif /* YKcaresHeader_h */
